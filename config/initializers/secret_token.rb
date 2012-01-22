695bbee0e3c82f73231400536f68566cc1ef861e
Ashton Harris <ahdesignsonline@gmail.com>
2012-01-22 14:03:11 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailtome::Application.config.secret_token = '6d7f1b706cb1ac73bac441da51ec677b15f9bd6ac4858fecf81a4d344e518db9e75b45ba0d178ce32ec7034f4f903b8777fcb027152339f7710416003703bafe'
